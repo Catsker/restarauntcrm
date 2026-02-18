1405279914dfd65afc712f8a1d41814e43ae1e6a
Казаренко Павел Владимирович <pkasarenko@gmail.com>
2026-02-18 12:15:32 +0300

    fix: code format

--- src/components/RestaurantList/RestaurantList.tsx
+++ src/components/RestaurantList/RestaurantList.tsx
@@ -9,13 +9,11 @@ const RestaurantList: React.FC = () => {
   return (
     <div className="p-6 max-w-[300px]">
       <ul className="flex flex-col gap-4">
-        {restaurants.map((restaurant: RestaurantData) => {
-          return (
-            <li key={restaurant.address}>
-              <Restaurant restaurant={restaurant}/>
-            </li>
-          )
-        })}
+        {restaurants.map((restaurant: RestaurantData) => (
+          <li key={restaurant.address}>
+            <Restaurant restaurant={restaurant}/>
+          </li>
+        ))}
       </ul>
     </div>
   )

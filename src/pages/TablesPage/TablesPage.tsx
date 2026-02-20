e41e2697376c5d8fa151a55f6f7bf3e613572181
Казаренко Павел Владимирович <pkasarenko@gmail.com>
2026-02-20 17:34:04 +0300

    fix: deleted comment

--- src/pages/TablesPage/TablesPage.tsx
+++ src/pages/TablesPage/TablesPage.tsx
@@ -37,7 +37,7 @@ const TablesPage: React.FC = () => {
     navigate({
       to: '/tables',
       search: newFilter !== filterStatus
-        ? { filterStatus: newFilter }  // если есть значение - добавляем
+        ? { filterStatus: newFilter }
         : {}
     });
   };

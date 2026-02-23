4e96b464183e82102c58f275d1155cf9c9fa02b3
Казаренко Павел Владимирович <pkasarenko@gmail.com>
2026-02-23 14:49:06 +0300

    fix: mappers deleted

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,21 +1,20 @@
-import type {TableType, TableStatusFull} from "@/types"
-import {TABLE_STATUS} from "@/types"
+import type {TableType, TableStatus} from "@/types"
 
 interface Props {
   table: TableType;
 }
 
-const variantClasses: Record<TableStatusFull, string> = {
-  Occupied: 'bg-[#FFF9E2] after:content-["Occ"] after:bg-[#FFE68E]',
-  Reserved: 'bg-[#FFE8E8] after:content-["Res"] after:bg-[#FF5858]',
-  Free: 'bg-white after:content-["Free"] after:bg-[#D3E79D]',
+const variantClasses: Record<TableStatus, string> = {
+  occupied: 'bg-[#FFF9E2] after:content-["Occ"] after:bg-[#FFE68E]',
+  reserved: 'bg-[#FFE8E8] after:content-["Res"] after:bg-[#FF5858]',
+  free: 'bg-white after:content-["Free"] after:bg-[#D3E79D]',
 }
 
 const Table = ({table}: Props) => {
   return (
     <div className={`
         border border-[#B1B1B1] h-[119px] relative p-2 rounded-2xl overflow-hidden after:rounded-bl-xl after:font-bold after:absolute after:top-0 after:right-0 after:p-1
-        ${variantClasses[TABLE_STATUS[table.status] as TableStatusFull]}
+        ${variantClasses[table.status]}
       `}>
       <span className="font-bold">{table.tableNumber}</span>
 
--- src/utils/tableStatistics.ts
+++ src/utils/tableStatistics.ts
@@ -3,16 +3,16 @@ import type { TableType, statisticsType } from '@/types';
 export const calculateTableStatistics = (tables: TableType[]): statisticsType[] => {
   return [
     {
-      type: 'Free',
-      count: tables.reduce((acc, cur) => cur.status === "Free" ? acc + 1 : acc, 0)
+      type: 'free',
+      count: tables.reduce((acc, cur) => cur.status === "free" ? acc + 1 : acc, 0)
     },
     {
-      type: 'Occ',
-      count: tables.reduce((acc, cur) => cur.status === "Occ" ? acc + 1 : acc, 0)
+      type: 'occupied',
+      count: tables.reduce((acc, cur) => cur.status === "occupied" ? acc + 1 : acc, 0)
     },
     {
-      type: 'Res',
-      count: tables.reduce((acc, cur) => cur.status === "Res" ? acc + 1 : acc, 0)
+      type: 'reserved',
+      count: tables.reduce((acc, cur) => cur.status === "reserved" ? acc + 1 : acc, 0)
     }
   ];
 };
